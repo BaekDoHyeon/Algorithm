66e23c9628f43ffce82a1ec3167e4687192eb299
BaekDoHyeon <qorehgus0823@naver.com>
2024-12-14 21:58:05 +0900

    add: [30802] 웰컴 키트

---
+++ src/백준/Class2/웰컴키트_30802.java
@@ -0,0 +1,28 @@
+package 백준.Class2;
+
+import java.util.Scanner;
+
+public class 웰컴키트_30802 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt(); //23
+        int[] tShirts = new int[6];
+        for (int i = 0; i < tShirts.length; i++)
+            tShirts[i] = sc.nextInt();
+        double T = sc.nextDouble(); //5
+        double P = sc.nextDouble(); //7
+
+        //티셔츠 묶음 구하기.
+        int tShirtsBundleCnt = 0;
+        for (int tShirt : tShirts) tShirtsBundleCnt += (int) Math.ceil(tShirt/T);
+        sb.append(tShirtsBundleCnt).append("\n");
+
+        //펜 최대 묶음과 몇 자루 주문 구하기.
+        sb.append((int) (N/P)).append(" ");
+        sb.append((int) (N%P)).append(" ");
+
+        System.out.println(sb);
+    }
+}
